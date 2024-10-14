1d21606a80c00e58ea4d9f2187073bdef3e1e61e
Delta Student <souravkr9012@gmail.com>
2024-10-15 01:32:14 +0530

    first commit

--- src/LudoBoard.jsx
+++ src/LudoBoard.jsx
@@ -4,9 +4,10 @@ import ludoImage from './ludo.png';
 
 export default function LudoBoard() {
     let [moves, setMoves] = useState({ blue: 0, green: 0, red: 0, yellow: 0 });
+    let [arr, SetArr] = useState(["no moves"]);
 
     let updateBlue = () => {
-        console.log(`moves.blue = ${moves.blue}`);
+        SetArr((prevArr) => [...prevArr, "blue moves"]);
         setMoves((prevMoves) => ({
             ...prevMoves,
             blue: prevMoves.blue + 1
@@ -14,6 +15,7 @@ export default function LudoBoard() {
     };
 
     let updateYellow = () => {
+        SetArr((prevArr) => [...prevArr, "yellow moves"]);
         setMoves((prevMoves) => ({
             ...prevMoves,
             yellow: prevMoves.yellow + 1
@@ -21,6 +23,7 @@ export default function LudoBoard() {
     };
 
     let updateGreen = () => {
+        SetArr((prevArr) => [...prevArr, "green moves"]);
         setMoves((prevMoves) => ({
             ...prevMoves,
             green: prevMoves.green + 1
@@ -28,6 +31,7 @@ export default function LudoBoard() {
     };
 
     let updateRed = () => {
+        SetArr((prevArr) => [...prevArr, "red moves"]);
         setMoves((prevMoves) => ({
             ...prevMoves,
             red: prevMoves.red + 1
@@ -48,6 +52,7 @@ export default function LudoBoard() {
             color: 'white',
         }}>
             <h1>Game Begins!</h1>
+            <h2>{arr.join(", ")}</h2>
             <div className="board">
                 <h3>Blue moves = {moves.blue}</h3>
                 <button style={{ backgroundColor: "blue" }} onClick={updateBlue}>+1</button>
